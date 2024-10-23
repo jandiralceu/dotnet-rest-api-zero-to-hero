2e0628f83ee601f78fad1f953490d2d51de98b12
Jandir A. Cutabiala <me@jandir.co>
2024-10-23 02:53:11 -0700

    feat: create movie flow

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        public MoviesController(IMovieRepository movieRepository) 
+        { 
+            _movieRepository = movieRepository;
+        }
+
+        private readonly IMovieRepository _movieRepository;
+
+        [HttpPost("movies")]
+        public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+        {
+            var movie = new Movie
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                YearOfRelease = request.YearOfRelease,
+                Genres = request.Genres.ToList(),
+            };
+
+            await _movieRepository.CreateAsync(movie);
+
+            return Created($"/api/movies/{movie.Id}", movie);
+        }
+    }
+}
--- Movies.Contracts/Requests/UpdateMovieRequest.cs
+++ Movies.Contracts/Requests/UpdateMovieRequest.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Movies.Contracts.Requests
+﻿namespace Movies.Contracts.Requests
 {
     public class UpdateMovieRequest
     {
         public required string Title { get; init; }
         public required int YearOfRelease { get; init; }
-        public required IEnumerable<string> Gennres { get; init; } = Enumerable.Empty<string>();
+        public required IEnumerable<string> Genres { get; init; } = [];
     }
 }
