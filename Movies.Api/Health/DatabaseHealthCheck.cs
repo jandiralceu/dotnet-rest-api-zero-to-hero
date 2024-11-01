c3d5e5f339621b3c72c51936004c1b4e43a15f79
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-11-01 18:19:59 -0300

    feat: implement health check for api

---
+++ Movies.Api/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Movies.Application.Database;
+
+namespace Movies.Api.Health;
+
+public class DatabaseHealthCheck : IHealthCheck
+{
+    public const string Name = "Database";
+    
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+    private readonly ILogger<DatabaseHealthCheck> _logger;
+
+    public DatabaseHealthCheck(IDbConnectionFactory dbConnectionFactory, ILogger<DatabaseHealthCheck> logger)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+        _logger = logger;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+    {
+        try
+        {
+            _ = await _dbConnectionFactory.CreateConnectionAsync(cancellationToken);
+            return HealthCheckResult.Healthy();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Database health check failed");
+            return HealthCheckResult.Unhealthy("Database health check failed", e);
+        }
+    }
+}
