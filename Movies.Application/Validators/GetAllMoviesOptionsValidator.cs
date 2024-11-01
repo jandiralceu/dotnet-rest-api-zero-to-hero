f084f25ab891a281e019eea5196e212004741952
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-10-31 23:15:27 -0300

    feat: validate page and pagesize params

--- Movies.Application/Validators/GetAllMoviesOptionsValidator.cs
+++ Movies.Application/Validators/GetAllMoviesOptionsValidator.cs
@@ -15,5 +15,12 @@ public class GetAllMoviesOptionsValidator : AbstractValidator<GetAllMoviesOption
         RuleFor(x => x.SortField)
             .Must(x => x is null || AcceptableSortFields.Contains(x, StringComparer.OrdinalIgnoreCase))
             .WithMessage("Invalid sort field. Acceptable values are: 'title', 'yearofrelease'");
+
+        RuleFor(x => x.Page)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 25)
+            .WithMessage("Page size must be between 1 and 25");
     }
 }
