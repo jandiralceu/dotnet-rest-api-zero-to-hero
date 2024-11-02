b30b85d3572bd625ed10b2bb72c1f50c166e7144
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-11-02 20:43:50 -0300

    feat: generate token

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,8 +1,38 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
+using Movies.Api.Sdk.Consumer;
+using Movies.Contracts.Requests;
 using Refit;
 
-var moviesApi = RestService.For<IMoviesApi>("http://localhost:5001");
+var services = new ServiceCollection();
 
-var movie = await moviesApi.GetMoviesAsync("die-welle-2008");
+services
+    .AddHttpClient()
+    .AddSingleton<AuthTokenProvider>()
+    .AddRefitClient<IMoviesApi>(s => new RefitSettings
+    {
+        AuthorizationHeaderValueGetter = async (_, __) => await s.GetRequiredService<AuthTokenProvider>().GetTokenAsync()
+    })
+    .ConfigureHttpClient(x =>
+        x.BaseAddress = new Uri("http://localhost:5001"));
+
+var provider = services.BuildServiceProvider();
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+var movie = await moviesApi.GetMovieAsync("die-welle-2008");
 Console.WriteLine(JsonSerializer.Serialize(movie));
+
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3
+};
+var movies = await moviesApi.GetMoviesAsync(request);
+foreach (var m in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(m));
+}
--- Movies.Api.Sdk/IMoviesApi.cs
+++ Movies.Api.Sdk/IMoviesApi.cs
@@ -1,10 +1,15 @@
+using Movies.Contracts.Requests;
 using Movies.Contracts.Responses;
 using Refit;
 
 namespace Movies.Api.Sdk;
 
+[Headers("Authorization: Bearer")]
 public interface IMoviesApi
 {
     [Get(ApiEndpoints.Movies.Get)]
-    Task<MovieResponse> GetMoviesAsync(string idOrSlug);
+    Task<MovieResponse> GetMovieAsync(string idOrSlug);
+    
+    [Get(ApiEndpoints.Movies.GetAll)]
+    Task<MoviesResponse> GetMoviesAsync(GetAllMoviesRequest request);
 }
