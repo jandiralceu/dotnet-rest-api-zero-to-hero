77475f6d7777a7f537f1f24f93f4ad072110ed69
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-11-02 21:08:43 -0300

    feat: create all endpoints for sdk

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -20,9 +20,13 @@ services
 var provider = services.BuildServiceProvider();
 var moviesApi = provider.GetRequiredService<IMoviesApi>();
 
+// Fetch a movie
+Console.WriteLine("Fetching a movie...");
 var movie = await moviesApi.GetMovieAsync("die-welle-2008");
 Console.WriteLine(JsonSerializer.Serialize(movie));
+Console.WriteLine();
 
+// Fetch all movies
 var request = new GetAllMoviesRequest
 {
     Title = null,
@@ -31,8 +35,37 @@ var request = new GetAllMoviesRequest
     Page = 1,
     PageSize = 3
 };
+Console.WriteLine("Fetching all movies...");
 var movies = await moviesApi.GetMoviesAsync(request);
 foreach (var m in movies.Items)
 {
     Console.WriteLine(JsonSerializer.Serialize(m));
 }
+Console.WriteLine();
+
+// Create a movie
+var newMovie = await moviesApi.CreateMovieAsync(new CreateMovieRequest
+{
+    Title = "The Shawshank Redemption",
+    YearOfRelease = 2000,
+    Genres = ["action", "drama"]
+});
+Console.WriteLine("Created movie:");
+Console.WriteLine(JsonSerializer.Serialize(newMovie));
+Console.WriteLine();
+
+// Update a movie
+var updatedMovie = await moviesApi.UpdateMovieAsync(newMovie.Id, new UpdateMovieRequest
+{
+    Title = "The Shawshank Redemption",
+    YearOfRelease = 1994,
+    Genres = ["drama"]
+});
+Console.WriteLine("Updated movie:");
+Console.WriteLine(JsonSerializer.Serialize(updatedMovie));
+Console.WriteLine();
+
+// Delete a movie
+Console.WriteLine($"Deleting movie {updatedMovie.Title}");
+await moviesApi.DeleteMovieAsync(updatedMovie.Id);
+Console.WriteLine("Movie deleted.");
